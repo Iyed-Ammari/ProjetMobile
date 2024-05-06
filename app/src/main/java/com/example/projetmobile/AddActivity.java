dc1e018ca0b9a93f7ed3a7fb136edcd6ff129cd8
Iyed Ammari <ammariiyed@isamm.u-manouba.tn>
2024-05-06 04:18:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projetmobile/AddActivity.java
@@ -0,0 +1,44 @@
+package com.example.projetmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class AddActivity extends AppCompatActivity {
+    FloatingActionButton back_button;
+    EditText title_input, author_input, pages_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_input);
+        add_button = findViewById(R.id.update_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim(),
+                        Integer.valueOf(pages_input.getText().toString().trim()));
+            }
+        });
+    }
+}
